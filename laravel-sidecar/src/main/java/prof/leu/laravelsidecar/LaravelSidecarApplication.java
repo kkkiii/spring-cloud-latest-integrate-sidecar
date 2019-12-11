f78fa08730b083e801abde85a60ef041dfbfe1c8
liuhongshuo <2624886248@qq.com>
2019-12-11 14:12:13 +0800

    first commit

---
+++ laravel-sidecar/src/main/java/prof/leu/laravelsidecar/LaravelSidecarApplication.java
@@ -0,0 +1,15 @@
+package prof.leu.laravelsidecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.sidecar.EnableSidecar;
+
+@SpringBootApplication
+@EnableSidecar
+public class LaravelSidecarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaravelSidecarApplication.class, args);
+    }
+
+}
---
+++ laravel-sidecar/src/test/java/prof/leu/laravelsidecar/LaravelSidecarApplicationTests.java
@@ -0,0 +1,13 @@
+package prof.leu.laravelsidecar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaravelSidecarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
